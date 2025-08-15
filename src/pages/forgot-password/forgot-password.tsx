fde0ab8556a70bba88efcccc2c5c60b1d5efddc0
Ihar Skavysh <igogoblin@gmail.com>
2025-08-15 21:40:13 +0300

    feat: add forgot password page layout

--- src/pages/forgot-password/forgot-password.tsx
+++ src/pages/forgot-password/forgot-password.tsx
@@ -1,15 +1,31 @@
-import { Button } from '@krgaa/react-developer-burger-ui-components';
+import { Button, Input } from '@krgaa/react-developer-burger-ui-components';
+import { useState } from 'react';
 
 export const ForgotPassword = (): React.JSX.Element => {
+  const [valueEmail, setValueEmail] = useState('');
   return (
-    <div>
+    <div
+      style={{
+        margin: 'auto auto',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: '24px',
+      }}
+    >
       <p className="text text_type_main-medium">Восстановление пароля</p>
+      <Input
+        type={'email'}
+        placeholder={'Укажите e-mail'}
+        value={valueEmail}
+        onChange={(e) => setValueEmail(e.target.value)}
+      />
       <Button htmlType={'button'} type={'primary'} size={'medium'}>
         Восстановить
       </Button>
       <p className="text text_type_main-default text_color_inactive mt-20">
         Вспомнили пароль?
-        <Button htmlType={'button'} type={'secondary'} size={'small'}>
+        <Button htmlType={'button'} type={'secondary'} size={'small'} extraClass="pl-2">
           Войти
         </Button>
       </p>
--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,3 +1,9 @@
 export const NotFound = (): React.JSX.Element => {
-  return <div>NotFound</div>;
+  return (
+    <div style={{ margin: 'auto auto', textAlign: 'center' }}>
+      <p className="text text_type_digits-large">404</p>
+      <p className="text text_type_main-large">NotFound</p>
+      <p className="text text_type_main-default">Страница не найдена</p>
+    </div>
+  );
 };
